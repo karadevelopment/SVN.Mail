d7f1cec67f4feace4304ad9b6a6557a7b2130618
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:42:23 +0100

    1.0.5.0

---
+++ Clients/SMTP.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace SVN.Mail.Clients
+{
+    public class SMTP : IDisposable
+    {
+        public string Host { get; }
+        public int Port { get; }
+        public bool EnableSsl { get; }
+        public string MailFrom { get; }
+        public string Email { get; }
+        private NetworkCredential Credentials { get; }
+
+        public SMTP(string host, int port, bool enableSsl, string mailFrom, string email, string password)
+        {
+            this.Host = host;
+            this.Port = port;
+            this.EnableSsl = enableSsl;
+            this.MailFrom = mailFrom;
+            this.Email = email;
+            this.Credentials = new NetworkCredential(email, password);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Send(string email, string subject, string body, bool isBodyHtml = false)
+        {
+            using (var mail = new MailMessage())
+            using (var client = new SmtpClient())
+            {
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.BodyEncoding = Encoding.UTF8;
+                mail.IsBodyHtml = isBodyHtml;
+                mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                mail.From = new MailAddress(this.Email, this.MailFrom);
+                mail.To.Add(email);
+
+                client.Host = this.Host;
+                client.Port = this.Port;
+                client.EnableSsl = this.EnableSsl;
+
+                client.UseDefaultCredentials = false;
+                client.Credentials = this.Credentials;
+
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Timeout = (int)TimeSpan.FromMinutes(1).TotalMilliseconds;
+
+                client.Send(mail);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SVN.Mail")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SVN Development")]
+[assembly: AssemblyProduct("SVN.Mail")]
+[assembly: AssemblyCopyright("Copyright © Muhammed Kara 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b6fb4f0b-7a2d-4bad-9f03-cc8436dbea76")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
