0a6128d92ffaa3f6c2198f271e6d0ef4ee0fc0dd
svndevelopment <47905966+svndevelopment@users.noreply.github.com>
2019-03-31 14:00:48 +0200

    1.1.1.0

---
+++ Clients/ImapClient.cs
@@ -0,0 +1,89 @@
+﻿using S22.Imap;
+using SVN.Mail.DataTransferObjects;
+using SVN.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+
+namespace SVN.Mail.Clients
+{
+    public class ImapClient : IDisposable
+    {
+        public string Host { get; }
+        public int Port { get; }
+        public bool EnableSsl { get; }
+        public string Email { get; }
+        private string Password { get; }
+
+        public ImapClient(string host, int port, bool enableSsl, string email, string password)
+        {
+            this.Host = host;
+            this.Port = port;
+            this.EnableSsl = enableSsl;
+            this.Email = email;
+            this.Password = password.Encrypt();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private IEnumerable<AttachementDto> GetAttachements(IEnumerable<Attachment> attachements)
+        {
+            foreach (var attachement in attachements)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    attachement.ContentStream.CopyTo(stream);
+
+                    yield return new AttachementDto
+                    {
+                        Name = attachement.Name.Substring(0, attachement.Name.IndexOf('.')),
+                        Extension = attachement.Name.Remove(0, attachement.Name.IndexOf('.')),
+                        Data = stream.ToArray(),
+                    };
+                }
+            }
+        }
+
+        private MailDto CreateMail(MailMessage message, string uid)
+        {
+            var dateTimeString = message.Headers["Date"];
+            var dateTimeParsed = DateTime.TryParse(dateTimeString, out var dateTime);
+
+            var result = new MailDto
+            {
+                Identifier = uid,
+                DateTime = dateTimeParsed ? dateTime : DateTime.Now,
+                MailFrom = message.From.DisplayName,
+                Email = message.From.Address,
+                Subject = message.Subject,
+                Body = message.Body,
+                IsHtml = message.IsBodyHtml,
+                Attachements = this.GetAttachements(message.Attachments.ToList()).ToList(),
+            };
+            return result;
+        }
+
+        public IEnumerable<MailDto> ReceiveAsync(params string[] identifiers)
+        {
+            using (var client = new S22.Imap.ImapClient(this.Host, this.Port, this.EnableSsl))
+            {
+                client.Login(this.Email, this.Password.Decrypt(), AuthMethod.Login);
+
+                var uids = client.Search(SearchCondition.All());
+                var identifiersAsUid = identifiers.Select(x => Convert.ToUInt32(x));
+
+                foreach (var uid in uids.Where(x => !identifiersAsUid.Contains(x)))
+                {
+                    var message = client.GetMessage(uid);
+                    yield return this.CreateMail(message, uid.ToString());
+                }
+
+                client.Logout();
+            }
+        }
+    }
+}
---
+++ Clients/Pop3Client.cs
@@ -0,0 +1,108 @@
+﻿using OpenPop.Mime;
+using SVN.Core.Linq;
+using SVN.Mail.DataTransferObjects;
+using SVN.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVN.Mail.Clients
+{
+    public class Pop3Client : IDisposable
+    {
+        public string Host { get; }
+        public int Port { get; }
+        public bool EnableSsl { get; }
+        public string Email { get; }
+        private string Password { get; }
+
+        public Pop3Client(string host, int port, bool enableSsl, string email, string password)
+        {
+            this.Host = host;
+            this.Port = port;
+            this.EnableSsl = enableSsl;
+            this.Email = email;
+            this.Password = password.Encrypt();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private IEnumerable<string> GetTexts(MessagePart part)
+        {
+            if (part.Body != null && !part.IsAttachment)
+            {
+                yield return Encoding.UTF8.GetString(part.Body);
+            }
+
+            foreach (var part2 in part.MessageParts ?? new List<MessagePart>())
+            {
+                foreach (var result in this.GetTexts(part2))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        private IEnumerable<AttachementDto> GetAttachements(MessagePart part)
+        {
+            if (part.Body != null && part.IsAttachment)
+            {
+                yield return new AttachementDto
+                {
+                    Name = part.FileName.Substring(0, part.FileName.IndexOf('.')),
+                    Extension = part.FileName.Remove(0, part.FileName.IndexOf('.')),
+                    Data = part.Body,
+                };
+            }
+
+            foreach (var part2 in part.MessageParts ?? new List<MessagePart>())
+            {
+                foreach (var result in this.GetAttachements(part2))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        private MailDto CreateMail(Message message, string uid)
+        {
+            var result = new MailDto
+            {
+                Identifier = uid,
+                DateTime = message.Headers.DateSent,
+                MailFrom = message.Headers.From.DisplayName,
+                Email = message.Headers.From.Address,
+                Subject = message.Headers.Subject,
+                Body = this.GetTexts(message.MessagePart).ToList().Join("\n"),
+                IsHtml = false,
+                Attachements = this.GetAttachements(message.MessagePart).ToList(),
+            };
+            return result;
+        }
+
+        public IEnumerable<MailDto> ReceiveAsync(params string[] identifiers)
+        {
+            using (var client = new OpenPop.Pop3.Pop3Client())
+            {
+                client.Connect(this.Host, this.Port, this.EnableSsl);
+                client.Authenticate(this.Email, this.Password.Decrypt());
+
+                var uids = client.GetMessageUids();
+                for (var i = 1; i <= uids.Count; i++)
+                {
+                    var uid = uids[i - 1];
+                    if (!identifiers.Contains(uid))
+                    {
+                        var message = client.GetMessage(i);
+                        yield return this.CreateMail(message, uid);
+                    }
+                }
+
+                client.Disconnect();
+            }
+        }
+    }
+}
